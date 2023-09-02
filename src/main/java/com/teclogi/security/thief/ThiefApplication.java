88951378fc92bbcbb93193860214d69f471b8b47
jmms11 <angiesv33@gmail.com>
2023-09-01 23:03:53 -0500

    complete second commit

---
+++ src/main/java/com/teclogi/security/thief/ThiefApplication.java
@@ -0,0 +1,13 @@
+package com.teclogi.security.thief;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThiefApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThiefApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/teclogi/security/thief/ThiefApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teclogi.security.thief;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThiefApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
